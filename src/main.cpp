4630ba651ef0cfd338e224b38065b8e9c39d8d5e
jay ess <mentoce@gmail.com>
2018-03-30 17:13:58 -0700

    Move build to use CMake

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "opencv2/features2d/features2d.hpp"
-#include "opencv2/nonfree/nonfree.hpp"
+#include <opencv2/nonfree/nonfree.hpp>
 #include "opencv2/flann/flann.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/opencv.hpp>
@@ -10,7 +10,7 @@
 #include <cmath>
 #include <fstream>
 #include <time.h>
-#include <videostab.h>
+#include "videostab.h"
 
 using namespace std;
 using namespace cv;
